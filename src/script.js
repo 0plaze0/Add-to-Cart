f5009dca93bf8331a591cfa92be043e0aceb3772
plaze <banikakash131@gamil.com>
2023-05-07 19:13:21 +0530

    "Database added"

--- src/script.js
+++ src/script.js
@@ -1,5 +1,37 @@
-const form = document.getElementById("UserInput");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(document.getElementById("EnterText").value);
-});
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+const FIREBASE = {
+  databaseURL:
+    "https://add-to-cart-b6af7-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const initApp = () => {
+  //connect to database
+  const app = initializeApp(FIREBASE);
+  const database = getDatabase(app);
+  const cartDB = ref(database, "Items");
+  //Dom element
+  const addToCart = document.getElementById("AddToCart");
+  const userInput = document.getElementById("userInput");
+
+  //take user input
+  addToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const item = userInput.value;
+    push(cartDB, item);
+    console.log(`${userInput.value} added to database`);
+
+    clearInput();
+    S;
+  });
+};
+
+const clearInput = () => {
+  userInput.value = "";
+};
+
+initApp();
